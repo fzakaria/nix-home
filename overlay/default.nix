89e0c1d33382f9190b1ed8b098d0c54150c7b24e
Farid Zakaria <fmzakari@google.com>
2020-07-11 23:22:52 -0700

    Removed jruby & node from overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,33 +1,4 @@
-self: super:
-let
-  buildNodejs =
-    super.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> { };
-in {
-  jruby_9_2_9_0 = super.jruby.overrideAttrs (oldAtrrs: rec {
-    version = "9.2.9.0";
-    src = super.fetchurl {
-      url =
-        "https://s3.amazonaws.com/jruby.org/downloads/${version}/jruby-bin-${version}.tar.gz";
-      sha256 = "04grdf57c1dgragm17yyjk69ak8mwiwfc1vjzskzcaag3fwgplyf";
-    };
-    # Apply this patch if you want the default GEM_HOME to be the user directory
-    #patches = [ ./pkgs/ruby/jruby-9.2.9.0.patch ];
-  });
-
-  jruby_9_2_12_0 = super.jruby.overrideAttrs (oldAtrrs: rec {
-    version = "9.2.12.0";
-    src = super.fetchurl {
-      url =
-        "https://s3.amazonaws.com/jruby.org/downloads/${version}/jruby-bin-${version}.tar.gz";
-      sha256 = "013c1q1n525y9ghp369z1jayivm9bw8c1x0g5lz7479hqhj62zrh";
-    };
-  });
-
-  nodejs-12_13_0 = buildNodejs {
-    version = "12.13.0";
-    sha256 = "1xmy73q3qjmy68glqxmfrk6baqk655py0cic22h1h0v7rx0iaax8";
-  };
-
+self: super: {
   rbenv = super.callPackage ./pkgs/ruby/rbenv.nix { };
   ruby-build = super.callPackage ./pkgs/ruby/ruby-build.nix { };
 
