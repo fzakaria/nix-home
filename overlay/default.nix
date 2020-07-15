a581e44b6db17463d03f75c9b7752a7d69c20ce5
Farid Zakaria <fmzakari@google.com>
2020-07-15 15:53:43 -0700

    Added overlay to make cachix latest

--- overlay/default.nix
+++ overlay/default.nix
@@ -11,4 +11,6 @@ self: super: {
     rev = "4a62ec17e20ce0e738a8e5126b4298a73903b468";
     sha256 = "0n5a3rnv9qnnsrl76kpi6dmaxmwj1mpdd2g0b4n1wfimqfaz6gi1";
   }) { };
+
+  cachix = (import (fetchTarball "https://cachix.org/api/v1/install") {}).cachix;
 }
