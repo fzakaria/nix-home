1e071cb357fc647113f6e5f6d4c3cf04498a105d
Farid Zakaria <fmzakari@google.com>
2020-09-01 13:53:14 -0700

    Added home-manager to niv

--- machines/google/home.nix
+++ machines/google/home.nix
@@ -18,8 +18,6 @@ in {
   else
     [ ]) ++ [ ../../modules/common.nix];
 
-  nixpkgs.config.allowUnfree = true;
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
--- shell.nix
+++ shell.nix
@@ -1,1 +1,19 @@
-{ }
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {
+  	overlays = [
+  		(_:super: {
+  			niv = (super.callPackage(import sources.niv) {}).niv;
+  			home-manager = (super.callPackage(import sources.home-manager) {}).home-manager;
+  		})
+  	];
+  };
+in with pkgs;
+mkShell {
+  name = "nix-home-shell";
+  buildInputs = [
+  	niv
+  	nixfmt
+  	home-manager
+  ];
+}
