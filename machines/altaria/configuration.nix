91c2dc202b91b31bdef2a5558535e8ba8762a394
Farid Zakaria <fmzakari@google.com>
2020-09-01 14:27:47 -0700

    Moved hvm to hardware configuration

--- machines/altaria/configuration.nix
+++ machines/altaria/configuration.nix
@@ -1,10 +1,9 @@
 let nixpkgs = (import ../../nix/sources.nix).nixos;
 in {
   imports = [
+  	./hardware-configuration.nix
     (nixpkgs + "/nixos/modules/virtualisation/amazon-image.nix")
     ../../modules/platforms/nixos.nix
   ];
-
-  ec2.hvm = true;
 
 }
