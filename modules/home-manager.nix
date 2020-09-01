d95373d8565e01538874be449f94006328fc1940
Farid Zakaria <fmzakari@google.com>
2020-09-01 14:46:48 -0700

    Added sensible default packages I use

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,4 +1,32 @@
-{ config, pkgs, ... }:
-{
-
+{ config, pkgs, ... }: {
+  # Place packages here that are
+  home.packages = with pkgs; [
+    # Rust CLI Tools! I love rust.
+    exa
+    fd
+    fzf
+    ripgrep
+    bat
+    comma
+    nix-index
+    nix-diff
+    # https://github.com/zimbatm/h
+    h
+    autojump
+    # fonts
+    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+    dejavu_fonts
+    powerline-fonts
+    teleconsole
+    cachix
+    jrnl
+    asciinema
+    redo-apenwarr
+    jq
+    htop
+    tmux
+    nixfmt
+    gitAndTools.delta
+    gitAndTools.gitFull
+  ];
 }
