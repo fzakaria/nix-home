33addfa1dae441edf3a6e6312dea1a884c0fca58
Farid Zakaria <fmzakari@google.com>
2020-09-17 15:18:25 -0700

    Fixup tailscale module

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -1,15 +1,10 @@
 { config, pkgs, ... }:
-let nixpkgs = (import ../nix/sources.nix).nixos;
-in {
-
-  # We use tailscale to setup our VPN across our machines
-  imports = [ (nixpkgs + "/nixos/modules/services/networking/tailscale.nix") ];
-
+{
   services.tailscale = {
   	enable = true;
   };
 
   networking.firewall.allowedUDPPorts = [ 41641 ];
 
-  environment.systemPackages = [ nixpkgs.tailscale ];
+  environment.systemPackages = [ pkgs.tailscale ];
 }
