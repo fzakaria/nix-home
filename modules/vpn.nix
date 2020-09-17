c1c2e98bd8918d81cb64df80d4af1588a3fabe8a
Farid Zakaria <fmzakari@google.com>
2020-09-17 09:40:53 -0700

    Fix vpn.nix import

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let nixpkgs = (import ../../nix/sources.nix).nixos;
+in {
 
 	# We use tailscale to setup our VPN across our machines
 	imports = [ (nixpkgs + "/nixos/modules/services/networking/tailscale.nix") ];
