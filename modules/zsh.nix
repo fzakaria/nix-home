7a63dd65a1fd314ea22613accbed0f3eb7deba4c
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-05-29 22:21:35 -0700

    Added frg to zshrc

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -63,6 +63,21 @@
         ${pkgs.coreutils}/bin/numfmt --to=iec-i
       }
 
+      # Got this sweet function from HackerNews
+      function frg {
+          result=$(rg --ignore-case --color=always --line-number --no-heading "$@" |
+            fzf --ansi \
+                --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+                --delimiter ':' \
+                --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
+                --preview-window 'up,60%,border-bottom,+{2}+3/3,~3')
+          file=''${result%%:*}
+          linenumber=$(echo "''${result}" | cut -d: -f2)
+          if [[ -n "$file" ]]; then
+                  $EDITOR +"''${linenumber}" "$file"
+          fi
+        }
+
       # https://github.com/zimbatm/h
       # setup h
       eval "$(${pkgs.h}/bin/h --setup ~/code)"
