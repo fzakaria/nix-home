2d4d765eb0f60cfcc2d2875ce7f64d77c13d5ba1
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-05-31 08:58:20 -0700

    enable wayland

--- machines/nyx/configuration.nix
+++ machines/nyx/configuration.nix
@@ -44,7 +44,7 @@
   programs.ssh.startAgent = false;
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
