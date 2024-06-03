6761766fa9bf8d1b3b9cdf9c4173c4e0b777301b
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-06-03 16:20:38 -0700

    Added back zsh

---
+++ users/fmzakari/zsh/default.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.zsh = {
+    enable = false;
+
+    autocd = true;
+    enableCompletion = true;
+    history = {
+      ignoreDups = true;
+      ignoreSpace = true;
+      extended = true;
+      share = true;
+    };
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [];
+    };
+    historySubstringSearch = {
+      enable = true;
+    };
+    enableAutosuggestions = true;
+    sessionVariables = {
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    oh-my-zsh = {
+      enable = true;
+      # since we are using powerlevel10k the theme is not used
+      theme = "";
+      plugins = [];
+    };
+
+    initExtraFirst =
+      ''
+        fpath=( ${pkgs.autojump}/share/zsh/site-functions $fpath)
+      ''
+      + builtins.readFile ./zshrc;
+
+    shellAliases = {
+      "cat" = "bat --style=plain";
+      "pbcopy" = "xsel --clipboard --input";
+    };
+  };
+
+  home.file.".p10k.zsh".source = ./p10k.zsh;
+}
--- users/fzakaria/default.nix
+++ users/fzakaria/default.nix
@@ -4,12 +4,29 @@
   lib,
   ...
 }: {
-
   imports = [
     ../fmzakari
   ];
 
-  home.username = lib.mkForce "fzakaria";
-  home.homeDirectory = lib.mkForce "/Users/fzakaria";
-  programs.git.userEmail = lib.mkForce "fzakaria@confluent.io";
+  home = {
+    username = lib.mkForce "fzakaria";
+    homeDirectory = lib.mkForce "/Users/fzakaria";
+  };
+
+  programs = {
+    zsh.enable = true;
+
+    git.userEmail = lib.mkForce "fzakaria@confluent.io";
+    # A command-line fuzzy finder
+    # https://github.com/junegunn/fzf
+    fzf = {
+      enable = true;
+      changeDirWidgetCommand = "fd --color always --hidden --follow --exclude .git --type d";
+      changeDirWidgetOptions = ["--ansi --preview 'exa --color always --tree {} | head -500'"];
+      fileWidgetCommand = "fd --color always --type f --hidden --follow --exclude .git";
+      fileWidgetOptions = [
+        "--ansi --preview-window=right:60% --preview 'bat --style=plain --color=always --line-range :500 {}'"
+      ];
+    };
+  };
 }
