0d253a6f51b016f1bd856eef2c14c518dda7a3ea
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-08-07 15:52:44 -0700

    Remove password prompt for sudo

--- machines/kuato/configuration.nix
+++ machines/kuato/configuration.nix
@@ -10,16 +10,21 @@
     # had a big discussion on Matrix on which to use for the Raspberry Pi 4
     # looks like there are pi specific modules but they told me to not use them.
     # Also don't use the vendored Linux kernel and just use the regular one.
-    "${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel.nix"
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
     ../../modules/nix.nix
     outputs.nixosModules.vpn
     # Feedback from Matrix was to disable this and it's unecessary unless you are using
     # some esoteric hardware.
-    # inputs.nixos-hardware.nixosModules.raspberry-pi-4
+    inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
 
   # let's not build ZFS for the Raspberry Pi 4
   boot.supportedFilesystems.zfs = lib.mkForce false;
+  # compressing image when using binfmt is very time consuming
+  # disable it. Not sure why we want to compress anyways?
+  sdImage.compressImage = false;
+  # enable the touch screen
+  hardware.raspberry-pi."4".touch-ft5406.enable = true;
 
   # we don't import ../../modules/nixpkgs.nix since we don't want the overlay
   # rebuilding for the Raspberry Pi 4 is expensive; try to stick to what is in the cache.
@@ -34,6 +39,12 @@
           system = final.system;
           config.allowUnfree = true;
         };
+      })
+      # Workaround: https://github.com/NixOS/nixpkgs/issues/154163
+      # modprobe: FATAL: Module sun4i-drm not found in directory
+      (final: super: {
+        makeModulesClosure = x:
+          super.makeModulesClosure (x // {allowMissing = true;});
       })
     ];
   };
@@ -62,6 +73,7 @@
   environment.systemPackages = with pkgs; [
     vim
     git
+    libraspberrypi
     raspberrypi-eeprom
   ];
 
@@ -88,10 +100,6 @@
         # Enable the GNOME Desktop Environment
         gdm = {
           enable = true;
-
-          # TODO(fzakaria): google-chrome doesn't respect fractional scaling
-          # when wayland is toggled. Disable for now.
-          wayland = false;
         };
       };
     };
@@ -99,7 +107,7 @@
 
   # Normally we would re-use the same user configurations in the users directory
   # but since this is a Raspberry Pi 4, lets make a much smaller closure.
-  users.extraUsers.fmzakari = {
+  users.users.fmzakari = {
     isNormalUser = true;
     shell = pkgs.bash;
     extraGroups = ["wheel" "networkmanager"];
@@ -107,7 +115,19 @@
     openssh.authorizedKeys.keyFiles = [
       ../../users/fmzakari/keys
     ];
+    # Allow the graphical user to login without password
+    initialHashedPassword = "";
   };
+
+  security = {
+    sudo = {
+      enable = true;
+      wheelNeedsPassword = false;
+    };
+  };
+
+  # Allow the user to log in as root without a password.
+  users.users.root.initialHashedPassword = "";
 
   hardware.enableRedistributableFirmware = true;
   system.stateVersion = "23.11";
