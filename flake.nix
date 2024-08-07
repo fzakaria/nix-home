1996857fb48f27c5d1b163c2cf66b1d79e16905b
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-08-06 22:33:12 -0700

    Follow home-manager in agenix to reduce closure

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
     # agenix
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.inputs.home-manager.follows = "home-manager";
 
     # vscode-extensions
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
