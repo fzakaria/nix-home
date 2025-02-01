187a8ee89ddb06f8e693bace3b458283fdcaa340
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-02-01 12:35:44 -0800

    Added description to prompt

--- users/fmzakari/fish.nix
+++ users/fmzakari/fish.nix
@@ -20,6 +20,49 @@
         wraps = "bash";
       };
 
+      # Based on https://gist.github.com/hroi/d0dc0e95221af858ee129fd66251897e
+      fish_jj_prompt = ''
+        # Is jj installed?
+        if not command -sq jj
+            return 1
+        end
+
+        # Are we in a jj repo?
+        if not jj root --quiet &>/dev/null
+            return 1
+        end
+
+        # Generate prompt
+            jj log --ignore-working-copy --no-graph --color always -r @ -T '
+        surround(
+            " (",
+            ")",
+            separate(
+                " ",
+                bookmarks.join(", "),
+                coalesce(
+                    surround(
+                        "\"",
+                        "\"",
+                        if(
+                            description.first_line().substr(0, 24).starts_with(description.first_line()),
+                            description.first_line().substr(0, 24),
+                            description.first_line().substr(0, 23) ++ "…"
+                        )
+                    ),
+                    "(no description set)"
+                ),
+                change_id.shortest(),
+                commit_id.shortest(),
+                if(conflict, "(conflict)"),
+                if(empty, "(empty)"),
+                if(divergent, "(divergent)"),
+                if(hidden, "(hidden)"),
+            )
+        )
+        '
+      '';
+
       # disable welcome message
       fish_greeting = "fish_prompt";
       nix-closure-size = {
@@ -42,6 +85,17 @@
         if test -n "$file"
             $EDITOR +"$linenumber" "$file"
         end
+      '';
+
+      fish_vcs_prompt = ''
+        fish_jj_prompt $argv
+        or fish_git_prompt $argv
+        or fish_hg_prompt $argv
+        or fish_fossil_prompt $argv
+        # The svn prompt is disabled by default because it's quite slow on common svn repositories.
+        # To enable it uncomment it.
+        # You can also only use it in specific directories by checking $PWD.
+        # or fish_svn_prompt
       '';
     };
 
