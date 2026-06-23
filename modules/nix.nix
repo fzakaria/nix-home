ebbfb87e0a763bcc019b4072e307bb1bda3d1076
Farid Zakaria <farid.m.zakaria@gmail.com>
2026-06-23 10:18:07 -0700

    Added numtide cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -31,10 +31,12 @@
       substituters = [
         "http://fzakaria.cachix.org"
         "https://nix-community.cachix.org"
+        "https://cache.numtide.com"
       ];
       trusted-public-keys = [
         "fzakaria.cachix.org-1:qWCiyGu0EmmRlo65Ro7b+L/QB0clhdeEofPxTOkRNng="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
   };
